1251ff3015688960e93aa4567e93e7a05a849c6e
robertrancz <rancz.robi@gmail.com>
2019-11-14 21:37:09 +0200

    Initial commit

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit, OnDestroy {
+  ingredients: Ingredient[];
+  private ingredientsChanged: Subscription;
+
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit() {
+    this.ingredients = this.slService.getIngredients();
+    this.ingredientsChanged = this.slService.ingredientsChanged
+      .subscribe(
+        (ingredients: Ingredient[]) => {
+          this.ingredients = ingredients;
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.ingredientsChanged.unsubscribe();
+  }
+}
